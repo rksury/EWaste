9e613f0837ed105e4b01d29c5d25af0a68c61ca3
Rksury <rksury@gmail.com>
2020-10-07 04:40:42 +0530

    update detail page

--- screens/ConsignmentDetail.tsx
+++ screens/ConsignmentDetail.tsx
@@ -1,5 +1,6 @@
 import *as React from 'react'
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {Card} from "react-native-paper";
 
 class ConsignmentDetail extends React.Component {
     render() {
@@ -8,8 +9,34 @@ class ConsignmentDetail extends React.Component {
 
             <View style={styles.container}>
                 <ScrollView showsVerticalScrollIndicator={false}>
-                    <View style={styles.content}>
-                        <Text style={styles.header}>Consignment Detail</Text>
+                    <View style={styles.headerContent}>
+                        <Text style={styles.header}>Order Detail</Text>
+                    </View>
+                    <View style={styles.detailContent}>
+                        <Card style={styles.Card}>
+                            <Card.Content>
+                                <Text style={styles.heading}>Order No.</Text>
+                                <Text style={styles.title}>Date</Text>
+                                <Text style={styles.title}>Vehicle No</Text>
+                                <Text style={styles.title}>Final Weight</Text>
+                                <Text style={styles.status}>Documents</Text>
+                                <View style={styles.ImageContent}>
+                                    <Image style={styles.cardImage}
+                                           source={require('../assets/images/Truck.png')}/>
+                                    <Image style={styles.cardImage}
+                                           source={require('../assets/images/Truck.png')}/>
+                                    <Image style={styles.cardImage}
+                                           source={require('../assets/images/Truck.png')}/>
+                                </View>
+                                <View style={styles.ImageContent}>
+                                    <Image style={styles.cardImage}
+                                           source={require('../assets/images/Truck.png')}/>
+                                    <Image style={styles.cardImage}
+                                           source={require('../assets/images/Truck.png')}/>
+
+                                </View>
+                            </Card.Content>
+                        </Card>
                     </View>
                 </ScrollView>
             </View>
@@ -22,23 +49,66 @@ export default ConsignmentDetail;
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        alignContent: 'stretch',
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#fff',
 
     },
-    content: {
+    headerContent: {
         backgroundColor: '#62bf4e',
         width: '100%',
         height: 80,
     },
-    header:{
+    header: {
         textAlign: 'center',
         fontSize: 20,
         color: '#ffffff',
-        marginTop:40,
-        fontWeight:'bold'
+        marginTop: 40,
+        fontWeight: 'bold'
+    },
+    detailContent: {
+        alignItems: 'center',
+    },
+    Card: {
+        width: '90%',
+        marginTop: 20,
+        marginBottom: 20,
+        borderRadius: 20,
+        shadowColor: "#979191",
+        shadowOffset: {width: 0, height: 10,},
+        shadowOpacity: 1.0,
+        shadowRadius: 13.16,
+        elevation: 10,
+        margin: 0,
+        padding: 0
+    },
 
-    }
+    heading: {
+        fontSize: 20,
+        color: '#878383'
+    },
+    title: {
+        fontSize: 14,
+        color: '#878383'
+    },
+    status: {
+        fontSize: 22,
+        color: '#878383',
+    },
+    ImageContent: {
+        flexDirection: 'row',
+        padding: 0,
+        width: '100%',
+        justifyContent: "center"
+    },
+    cardImage: {
+        width: 90,
+        height: 90,
+        margin: 8,
+        borderRadius: 20,
+        backgroundColor: '#efefef',
+
+    },
 })
 
 
